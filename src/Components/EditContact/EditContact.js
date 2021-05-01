3c05ed520b5c194d02048b6a51442f493fb9618c
Haritha1214 <65326618+Haritha1214@users.noreply.github.com>
2021-05-01 20:23:53 +0530

    Update EditContact.js

--- src/Components/EditContact/EditContact.js
+++ src/Components/EditContact/EditContact.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router";
 import {Link} from 'react-router-dom';
 import { useHistory } from 'react-router';
 
-const AddEdit = () => {
+const EditContact = () => {
   const { Id } = useParams();
   const history=useHistory();
   history.push("/")
